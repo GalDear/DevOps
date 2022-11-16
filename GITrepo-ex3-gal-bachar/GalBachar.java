32482a122216ae45278fef5914fead19acfee995
Gal Bachar <gal.bachar@cymotive.com>
2022-11-16 10:01:29 +0200

    ex3

--- GITrepo-ex3-gal-bachar/GalBachar.java
+++ GITrepo-ex3-gal-bachar/GalBachar.java
@@ -1,4 +1,4 @@
-public class gal-bachar {
+public class GalBachar {
     public static void main(String[] args) {
         for(int i=1; i <=10; i++){
             System.out.println("Gal Bachar..." + 1);
